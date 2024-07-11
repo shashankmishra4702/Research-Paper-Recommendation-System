b91d676c4640616347011b8b18ec926cbea6525f
comdershashank <105141896+shashankmishra4702@users.noreply.github.com>
2024-07-11 17:29:17 +0530

    Add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,25 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-// combineReducers,
+import {combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
-// import { persistReducer, persistStore } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// const rootReducer = combineReducers({ user: userReducer });
+//We have used reducer from react redux library to store the info of the user even after he refreshes the page
 
-// const persistConfig = {
-//   key: 'root',
-//   version: 1,
-//   storage,
-// };
+const rootReducer = combineReducers({ user: userReducer });
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
