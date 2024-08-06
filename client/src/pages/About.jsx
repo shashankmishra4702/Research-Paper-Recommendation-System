1aeac20baab4052d7ec912e35745776e51770d84
pandey2029 <107745010+pandey2029@users.noreply.github.com>
2024-08-06 20:41:46 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div className="flex items-center justify-center h-screen">
-  <p className="text-4xl">Raghav ki chuchi sabse uchi</p>
+  <p className="Our Recommender System works on a hybrid model utilising both content based filtering and collaborative based filtering.</p>
 </div>
 
   )
