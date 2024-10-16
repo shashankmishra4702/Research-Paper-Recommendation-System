f5ab9369a3c7194c17cc0a3b8fec550e33d58218
shashank_mishra <105141896+shashankmishra4702@users.noreply.github.com>
2024-10-16 22:12:50 +0530

    Update app.py

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -82,53 +82,7 @@ if __name__ == '__main__':
 
 
 
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# import pandas as pd
-# from gensim.models.doc2vec import Doc2Vec
-# from nltk.tokenize import word_tokenize
-# import pickle
 
-# # Initialize Flask app
-# app = Flask(__name__)
-# CORS(app)
-
-# # Load preprocessed data and model
-# model = Doc2Vec.load('doc2vec.model')
-# papers = pd.read_pickle('papers.pkl')
-# with open('citation_similarity.pkl', 'rb') as f:
-#     citation_similarity = pickle.load(f)
-
-# # Clean the abstracts column (if needed)
-# papers['abstract'] = papers['abstract'].fillna('')
-
-# # Extract abstracts and paper IDs
-# abstracts = papers['abstract'].tolist()
-
-# # Function to recommend similar abstracts
-# def recommend_papers(doc_id, top_n=5):
-#     inferred_vector = model.infer_vector(word_tokenize(abstracts[doc_id].lower()))
-#     content_sims = model.dv.most_similar([inferred_vector], topn=len(papers))
-#     content_sims_dict = {int(tag): sim for tag, sim in content_sims}
-
-#     max_content_sim = max(content_sims_dict.values())
-#     content_sims_dict = {k: v / max_content_sim for k, v in content_sims_dict.items()}
-
-#     citation_sims = citation_similarity[doc_id]
-#     citation_sims_dict = {i: citation_sims[i] for i in range(len(papers))}
-
-#     max_citation_sim = max(citation_sims_dict.values())
-#     if max_citation_sim > 0:
-#         citation_sims_dict = {k: v / max_citation_sim for k, v in citation_sims_dict.items()}
-#     else:
-#         citation_sims_dict = {k: 0 for k in citation_sims_dict}
-
-#     combined_sims = {i: content_sims_dict.get(i, 0) * 0.5 + citation_sims_dict.get(i, 0) * 0.5 for i in range(len(papers))}
-
-#     recommended_abstracts = sorted(combined_sims.items(), key=lambda item: item[1], reverse=True)
-#     recommended_abstracts = [(idx, sim) for idx, sim in recommended_abstracts if idx != doc_id][:top_n]
-
-#     return recommended_abstracts
 
 # @app.route('/recommend', methods=['POST'])
 # def recommend():
